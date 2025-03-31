8791c9c4dcb159934fea8d6929cfecc2f3b003f3
Jo Wagner <jowagner@limago.de>
2025-03-31 12:30:38 +0200

    initial commit

---
+++ Tag1_02Singleton/Singleton.h
@@ -0,0 +1,27 @@
+//
+// Created by JoachimWagner on 13.01.2025.
+//
+
+#pragma once
+
+
+#include <iostream>
+
+class Singleton {
+    Singleton() = default;
+
+public:
+    Singleton(Singleton const&)               = delete;
+    auto operator=(Singleton const&)->void    = delete;
+
+    Singleton(Singleton &&)               = delete;
+    auto operator=(Singleton &&)->void    = delete;
+
+    static auto create ()-> Singleton& {
+        static Singleton instance ; // Lazy + Threadsafe
+        return instance;
+    }
+    auto log(std::string message)->void{
+        std::cout << message << std::endl;
+    }
+};
